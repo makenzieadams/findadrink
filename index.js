31e7d621d34aa7761651f866c8815aba3f3bb7cc
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-02 22:02:10 -0500

    added cards for drink titles and CSS

--- index.js
+++ index.js
@@ -11,8 +11,55 @@ const setupPage = () => {
 
     const term = event.target.drink.value;
     getDrinks(term).then((drinks) => {
-      console.log(drinks);
+      fireConfetti();
+      showDrinks(drinks);
     });
+  });
+
+  const randomDrink = document.querySelector("#randomdrink");
+  randomDrink.addEventListener("click", (event) => {
+    const getRandomDrink = () => {
+      const options = {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      };
+
+      return fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/random.php`,
+        options
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          return res.drinks;
+        });
+    };
+
+    getRandomDrink().then((drinks) => {
+      fireConfetti();
+      showDrinks(drinks);
+    });
+  });
+};
+
+const showDrinks = (drinks) => {
+  const drinkContainer = document.querySelector("#drink-container");
+  while (drinkContainer.firstChild) {
+    drinkContainer.firstChild.remove();
+  }
+
+  drinks.forEach((drink) => {
+    const drinkCard = document.createElement("div");
+    drinkCard.classList.add("drink-card");
+    const drinkTitle = document.createElement("h5");
+
+    drinkTitle.textContent = drink.strDrink;
+
+    drinkCard.appendChild(drinkTitle);
+    drinkContainer.appendChild(drinkCard);
   });
 };
 
