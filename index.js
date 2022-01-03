8b237cb9c192fe2f2f97658cf53dd661bb6f9ccd
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-02 21:12:18 -0500

    added js and API

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const jsConfetti = new JSConfetti();
+const setupPage = () => {
+  const jsConfetti = new JSConfetti();
 
-jsConfetti.addConfetti();
+  const fireConfetti = () => {
+    jsConfetti.addConfetti();
+  };
+
+  const searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const term = event.target.drink.value;
+    getDrinks(term).then((drinks) => {
+      console.log(drinks);
+    });
+  });
+};
+
+const getDrinks = (term) => {
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`,
+    options
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      return res.drinks;
+    });
+};
+
+document.addEventListener("DOMContentLoaded", setupPage);
